72c4c42310aff5e209a2c92464ef234bc7c7f8fb
BabarHussainUBL <hussain.babar@ubl.com.pk>
2023-06-14 02:58:04 +0500

    add

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const config = {
   db: {
     host: process.env.MYSQLHOST || "localhost",
-    database: process.env.MYSQLDATABASE || "ecom_api_db",
-    user: process.env.MYSQLUSER || "root",
-    password: process.env.MYSQLPASSWORD || "root",
+    database: process.env.MYSQLDATABASE  || "ecom_api_db",
+    user:  process.env.MYSQLUSER || "root",
+    password:  process.env.MYSQLPASSWORD || "root",
   },
-  port:process.env.MYSQLPORT || 30004
+  port: process.env.MYSQLPORT || 30004
 };
 
 
